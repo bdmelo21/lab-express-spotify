cc12bf325929c09cf4ebf861f5f0c78dcddcfad1
Bernardo Melo <bernardodmelo@gmail.com>
2020-11-05 20:18:39 +0000

    done

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +11,51 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/',(req, res)=>{
+    res.render('index');
+});
+
+app.get('/artist-search',(req,res)=>{
+    let artists= req.query.artists;
+    spotifyApi
+  .searchArtists(artists)
+  .then(data => {
+    console.log('The received data from the API: ', data.body.artists.items);
+    res.render('artist-search-results', {artist: data.body.artists.items});
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+app.get('/albums/:artistId', (req, res, next) => {
+    let id = req.params.artistId;
+    spotifyApi
+    .getArtistAlbums(id)
+    .then (data=>{
+        console.log('The received data from the API:', data.body.items);
+        res.render('albums', {albums:data.body.items})
+    })
+  });
+
+  app.get('/tracks/:albumId', (req, res, next) => {
+    let newid = req.params.albumId;
+    spotifyApi
+    .getAlbumTracks(newid)
+    .then (data=>{
+        console.log('The received data from the API:', data.body.items);
+        res.render('tracks', {tracks:data.body.items})
+    })
+  });
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
